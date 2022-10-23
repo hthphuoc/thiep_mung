d76f2e168f15653eaf9a03e3ed3a2acd01a41f4d
hthphuoc <101106786+hthphuoc@users.noreply.github.com>
2022-10-23 17:57:04 +0700

    Add files via upload
    commit card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Sinh nhật 24/10';
+const titleCard = 'Tặng em';
+const contentCard = 'Chúc mừng sinh nhật em tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. I love you.';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'em.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
